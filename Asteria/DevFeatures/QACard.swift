110020b3a60cbdd47ce7648e3fc864301ae54b68
Anne Ferret <anneferret94@gmail.com>
2022-06-17 12:31:04 +0200

    beginning of the card

---
+++ Asteria/DevFeatures/QACard.swift
@@ -0,0 +1,22 @@
+//
+//  QACard.swift
+//  Asteria
+//
+//  Created by apprenant70 on 17/06/2022.
+//
+
+import SwiftUI
+
+struct QACard: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(Color("CyberGrape"))
+            .frame(width:300, height:600)
+    }
+}
+
+struct QACard_Previews: PreviewProvider {
+    static var previews: some View {
+        QACard()
+    }
+}
