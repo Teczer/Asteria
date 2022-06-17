ef76486b298d5a720c6645a1fe1b9f6962ae40de
Mehdi <mehdi.hattou1@gmail.com>
2022-06-17 14:08:15 +0200

    Changement 280 largeur à 270 (pour les boutons)

--- Asteria/DevFeatures/MehdiWIP.swift
+++ Asteria/DevFeatures/MehdiWIP.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct MehdiWIP: View {
+    @State var change: Bool = false
     var body: some View {
-        VStack {
-            CustomButton(colorOfButton: "pink", textInButton: "button")
+        NavigationView {
+            VStack {
+                CustomButton(colorOfButton: "blue", textInButton: "Connexion")
+                
+                
+                
+                
+                NavigationLink(isActive: $change, destination: {VeroWIP()}) {
+                    if change == false {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 280, height: 50)
+                            .foregroundColor(Color("CarnationPink"))
+                            .overlay(
+                                Text("textInButton")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("SpaceCadet"))
+                            )
+                    } else {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 280, height: 50)
+                            .foregroundColor(Color("CarnationPink"))
+                            .overlay(
+                                Text("textInButton")
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("SpaceCadet"))
+                            )
+                        
+                    }
+                }
+            }
         }
     }
 }
@@ -27,33 +56,38 @@ struct CustomButton: View {
     var colorOfButton : String
     var textInButton : String
     var body: some View {
-        
-        
         if colorOfButton == "pink" {
             RoundedRectangle(cornerRadius: 10)
-                .frame(width: 280, height: 50)
+                .frame(width: 270, height: 50)
                 .foregroundColor(Color("CarnationPink"))
                 .overlay(
                     Text(textInButton)
+                        .fontWeight(.bold)
                         .foregroundColor(Color("SpaceCadet"))
                 )
         } else if colorOfButton == "blue" {
             RoundedRectangle(cornerRadius: 10)
-                .frame(width: 280, height: 50)
+                .frame(width: 270, height: 50)
                 .foregroundColor(Color("SpaceCadet"))
                 .overlay(
                     Text(textInButton)
+                        .fontWeight(.bold)
                         .foregroundColor(Color("LavenderBlush"))
                 )
         } else if colorOfButton == "white" {
             RoundedRectangle(cornerRadius: 10)
-                .frame(width: 280, height: 50)
+                .frame(width: 270, height: 50)
                 .foregroundColor(.white)
                 .overlay(
                     Text(textInButton)
+                        .fontWeight(.bold)
                         .foregroundColor(Color("OxfordBlue"))
                 )
         }
     }
 }
+
+
+
+
 
