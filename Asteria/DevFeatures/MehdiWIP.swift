1923e0cc8f5d1ab478b2d5fc364bdb7228e1ea79
Mehdi <mehdi.hattou1@gmail.com>
2022-06-17 12:30:48 +0200

    Sauvegarde boutons

--- Asteria/DevFeatures/MehdiWIP.swift
+++ Asteria/DevFeatures/MehdiWIP.swift
@@ -9,12 +9,61 @@ import SwiftUI
 
 struct MehdiWIP: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ButtonEdit(textInButton: "Bouton", typeOfButton: false)
+            ButtonEdit(textInButton: "Bouton", typeOfButton: true)
+                .padding(.bottom)
+            WhiteButton(textInButton: "Bouton")
+            
+        }
     }
 }
 
 struct MehdiWIP_Previews: PreviewProvider {
     static var previews: some View {
         MehdiWIP()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 13")
     }
 }
+
+struct WhiteButton: View {
+    var textInButton : String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .frame(width: 280, height: 50)
+            .foregroundColor(.white)
+            .overlay(
+                Text(textInButton)
+                    .foregroundColor(Color("OxfordBlue"))
+            )
+    }
+}
+
+struct ButtonEdit: View {
+    var textInButton : String
+    var typeOfButton : Bool
+    
+    var body: some View {
+        
+        
+        if typeOfButton == true {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 280, height: 50)
+                .foregroundColor(Color("CarnationPink"))
+                .overlay(
+                    Text(textInButton)
+                        .foregroundColor(Color("SpaceCadet"))
+                )
+        } else {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 280, height: 50)
+                .foregroundColor(Color("SpaceCadet"))
+                .overlay(
+                    Text(textInButton)
+                        .foregroundColor(Color("LavenderBlush"))
+                )
+        }
+    }
+}
+
