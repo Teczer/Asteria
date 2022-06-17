3132508b866085492b64d158b3ea30a5911e7e2a
Anne Ferret <anneferret94@gmail.com>
2022-06-17 10:26:24 +0200

    working environment

---
+++ Asteria/DevFeatures/EllanaWIP.swift
@@ -0,0 +1,20 @@
+//
+//  EllanaWIP.swift
+//  Asteria
+//
+//  Created by apprenant70 on 17/06/2022.
+//
+
+import SwiftUI
+
+struct EllanaWIP: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EllanaWIP_Previews: PreviewProvider {
+    static var previews: some View {
+        EllanaWIP()
+    }
+}
