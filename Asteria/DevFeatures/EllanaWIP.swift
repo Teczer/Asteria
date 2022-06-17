48e403a817a7fa17d4a3c48ba5662517650220db
Anne Ferret <anneferret94@gmail.com>
2022-06-17 16:31:53 +0200

    lot of upgrades in the ProgressBar system

--- Asteria/DevFeatures/EllanaWIP.swift
+++ Asteria/DevFeatures/EllanaWIP.swift
@@ -7,78 +7,62 @@
 
 import SwiftUI
 
+//sample values
+let questionNoCurrent = 4
+let questionNoTotal = 5
+
+//unmutable values
+let questionProgression:Double = Double(questionNoCurrent-1)/(Double(questionNoTotal)-1)
+
 struct ProgressBar: View {
-    @Binding var value: Float
-    
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
-                HStack{
-                    Color("CarnationPink")
-                        .frame(width: geometry.size.width*0.75, height: 5)
-                    Color("LavenderBlush")
-                        .frame(width: geometry.size.width*0.25, height: 5)
-                        .opacity(0.5)
+                HStack (spacing:0) {
+                    Color("CarnationPink").opacity(0.8)
+                        .frame(width: geometry.size.width*CGFloat(questionProgression), height: 5)
+                    Color("LavenderBlush").opacity(0.8)
+                        .frame(height: 5)
                 }
-                HStack{
+                HStack {
                     Circle()
-                        .frame(width: 10, height: 10)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color("LavenderBlush"))
                     Spacer()
                     Circle()
-                        .frame(width: 10, height: 10)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color("LavenderBlush"))
                     Spacer()
                     Circle()
-                        .frame(width: 10, height: 10)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color("LavenderBlush"))
                     Spacer()
                     Circle()
-                        .frame(width: 10, height: 10)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color("LavenderBlush"))
                     Spacer()
                     Circle()
-                        .frame(width: 10, height: 10)
+                        .frame(width: 20, height: 20)
                         .foregroundColor(Color("LavenderBlush"))
                 }
             }
         }
-    }
-}
-struct EllaView: View {
-    @State var progressValue: Float = 0.0
-    
-    var body: some View {
-        VStack {
-            ProgressBar(value: $progressValue).frame(height: 20)
-            
-            Button(action: {
-                self.startProgressBar()
-            }) {
-                Text("Start Progress")
-            }.padding()
-            
-            Button(action: {
-                self.resetProgressBar()
-            }) {
-                Text("Reset")
-            }
-            
-            Spacer()
-        }.padding()
-    }
-    
-    func startProgressBar() {
-        for _ in 0...100 {
-            self.progressValue += 0.25
-        }
-    }
-    
-    func resetProgressBar() {
-        self.progressValue = 0.0
     }
 }
 
+struct EllaView: View {
+    
+    var body: some View {
+        VStack {
+            ProgressBar()
+            
+            Text("Progression \(questionProgression)")
+                .foregroundColor(Color.red)
+            
+        }.padding()
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
