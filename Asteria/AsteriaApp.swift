7a3aff31404e32c1d348a17f6245cbbf6cf053e7
Anne Ferret <anneferret94@gmail.com>
2022-06-17 10:12:14 +0200

    Initial Commit

---
+++ Asteria/AsteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsteriaApp.swift
+//  Asteria
+//
+//  Created by apprenant70 on 17/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AsteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
