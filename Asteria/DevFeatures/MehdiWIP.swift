34732a33cab4c4125c1c7eefaf73e219a2a0326b
Mehdi <mehdi.hattou1@gmail.com>
2022-06-17 15:27:29 +0200

    Ajout :
    Fix Answer Button :
    - Rectangle height
    - Text font, frame and size
    
    NavigationLink Button /TEST/

--- Asteria/DevFeatures/MehdiWIP.swift
+++ Asteria/DevFeatures/MehdiWIP.swift
@@ -8,99 +8,96 @@
 import SwiftUI
 
 struct MehdiWIP: View {
+    @State var changeA: Bool = false
     @State var change: Bool = false
     var body: some View {
         NavigationView {
             VStack {
-                AnswerButton(textInButton: "Bouton")
-                NavigationLink(isActive: $change, destination: {VeroWIP()}) {
-                    if change == false {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(width: 270, height: 50)
-                            .foregroundColor(Color("OxfordBlue"))
-                            .overlay(
-                                Text("NavigationLink")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.white)
-                            )
-                    } else {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(width: 270, height: 50)
-                            .foregroundColor(Color("CarnationPink"))
-                            .overlay(
-                                Text("NavigationLink")
-                                    .fontWeight(.bold)
-                                    .foregroundColor(Color("SpaceCadet"))
-                            )
-                        
+                AnswerButton(textInButton: "ce n’est pas un satellite")
+                NavigationLink(isActive: $change, destination: {VeroWIP()}) {}
+                
+                Button {
+                        self.changeA.toggle()
+                        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+                        self.change.toggle()
                     }
+                } label: {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 128, height: 50)
+                        .foregroundColor(changeA ? Color("CarnationPink") : Color("OxfordBlue"))
+                        .overlay(
+                            Text("ce n’est pas un satellite")
+                                .font(.system(size: 12))
+                                .frame(width: 73, height: 50)
+                                .foregroundColor(changeA ? Color("SpaceCadet") : Color.white )
+                        )
                 }
             }
         }
     }
-}
-
-struct MehdiWIP_Previews: PreviewProvider {
-    static var previews: some View {
-        MehdiWIP()
-            .preferredColorScheme(.dark)
-            .previewDevice("iPhone 13")
+    
+    struct MehdiWIP_Previews: PreviewProvider {
+        static var previews: some View {
+            MehdiWIP()
+                .preferredColorScheme(.dark)
+                .previewDevice("iPhone 13")
+        }
     }
-}
-
-struct CustomButton: View {
-    var colorOfButton : String
-    var textInButton : String
-    var body: some View {
-        if colorOfButton == "pink" {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: 270, height: 50)
-                .foregroundColor(Color("CarnationPink"))
-                .overlay(
-                    Text(textInButton)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("SpaceCadet"))
-                )
-        } else if colorOfButton == "blue" {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: 270, height: 50)
-                .foregroundColor(Color("SpaceCadet"))
-                .overlay(
-                    Text(textInButton)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("LavenderBlush"))
-                )
-        } else if colorOfButton == "white" {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: 270, height: 50)
-                .foregroundColor(.white)
-                .overlay(
-                    Text(textInButton)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("OxfordBlue"))
-                )
+    
+    struct CustomButton: View {
+        var colorOfButton : String
+        var textInButton : String
+        var body: some View {
+            if colorOfButton == "pink" {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 270, height: 50)
+                    .foregroundColor(Color("CarnationPink"))
+                    .overlay(
+                        Text(textInButton)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("SpaceCadet"))
+                    )
+            } else if colorOfButton == "blue" {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 270, height: 50)
+                    .foregroundColor(Color("SpaceCadet"))
+                    .overlay(
+                        Text(textInButton)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("LavenderBlush"))
+                    )
+            } else if colorOfButton == "white" {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 270, height: 50)
+                    .foregroundColor(.white)
+                    .overlay(
+                        Text(textInButton)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("OxfordBlue"))
+                    )
+            }
+        }
+    }
+    
+    struct AnswerButton: View {
+        @State var selectedButton : Bool = false
+        var textInButton : String
+        var body: some View {
+            Button {
+                self.selectedButton.toggle()
+            } label: {
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 128, height: 50)
+                    .foregroundColor(selectedButton ? Color("CarnationPink") : Color("OxfordBlue"))
+                    .overlay(
+                        Text(textInButton)
+                            .font(.system(size: 12))
+                            .frame(width: 73, height: 50)
+                            .foregroundColor(selectedButton ? Color("SpaceCadet") : Color.white )
+                    )
+            }
         }
     }
 }
-
-struct AnswerButton: View {
-    @State var selectedButton : Bool = false
-    var textInButton : String
-    var body: some View {
-        Button {
-            self.selectedButton.toggle()
-        } label: {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: 128, height: 58)
-                .foregroundColor(selectedButton ? Color("CarnationPink") : Color("OxfordBlue"))
-                .overlay(
-                    Text(textInButton)
-                        .fontWeight(.bold)
-                        .foregroundColor(selectedButton ? Color("SpaceCadet") : Color.white )
-                )
-        }
-    }
-}
-
 
 
