3bf22526b980de7bb749ed5941c115175c5a3072
ellanaca <ellanaca1592@gmail.com>
2022-06-17 14:59:15 +0200

    "last good version"

--- Asteria/DevFeatures/EllanaWIP.swift
+++ Asteria/DevFeatures/EllanaWIP.swift
@@ -7,50 +7,83 @@
 
 import SwiftUI
 
-struct EllanaWIP: View {
+struct ProgressBar: View {
+    @Binding var value: Float
+    
     var body: some View {
-        ZStack{
-            VStack(){
-                ZStack(){
-                    Color("LavenderBlush")
-                        .frame(width: 243, height: 5)
-                        .opacity(0.5)
-                    
-                    HStack(){
-                        
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color("LavenderBlush"))
-                        Spacer()
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color("LavenderBlush"))
-                        Spacer()
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color("LavenderBlush"))
-                        Spacer()
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color("LavenderBlush"))
-                        Spacer()
-                        Circle()
-                            .frame(width: 10, height: 10)
-                            .foregroundColor(Color("LavenderBlush"))
-                        
-                    }.padding(70)
-                }
-                Spacer()
-            }
-            Text("Question  sur 5")
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Color("LavenderBlush")
+                    .frame(width: geometry.size.width, height: 5)
+                    .opacity(0.5)
+                    .frame(width: geometry.size.width*1)
                 
+                Color("CarnationPink")
+                    .frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: 5)
+                    .animation(.linear)
+                HStack{
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color("LavenderBlush"))
+                    Spacer()
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color("LavenderBlush"))
+                    Spacer()
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color("LavenderBlush"))
+                    Spacer()
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color("LavenderBlush"))
+                    Spacer()
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundColor(Color("LavenderBlush"))
+                }
+            }
         }
     }
 }
+struct EllaView: View {
+    @State var progressValue: Float = 0.0
+    
+    var body: some View {
+        VStack {
+            ProgressBar(value: $progressValue).frame(height: 20)
+            
+            Button(action: {
+                self.startProgressBar()
+            }) {
+                Text("Start Progress")
+            }.padding()
+            
+            Button(action: {
+                self.resetProgressBar()
+            }) {
+                Text("Reset")
+            }
+            
+            Spacer()
+        }.padding()
+    }
+    
+    func startProgressBar() {
+        for _ in 0...100 {
+            self.progressValue += 0.25
+        }
+    }
+    
+    func resetProgressBar() {
+        self.progressValue = 0.0
+    }
+}
 
-struct EllanaWIP_Previews: PreviewProvider {
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        EllanaWIP()
+        EllaView()
             .preferredColorScheme(.dark)
     }
 }
