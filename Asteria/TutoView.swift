77e0d04a7f84216247a1b9a8030d86baf5b56c15
Anne Ferret <anneferret94@gmail.com>
2022-06-20 11:59:26 +0200

    basic implementation for TutoView

--- Asteria/TutoView.swift
+++ Asteria/TutoView.swift
@@ -6,13 +6,54 @@
 //
 
 import SwiftUI
+import Foundation   // needed for markdown formatting in text
 
 struct TutoView: View {
     var body: some View {
         ZStack {
-            Color.red
+            Color("CyberGrape")
                 .ignoresSafeArea()
-            
+            VStack {
+                Image("tutoscreen02")
+                    .resizable()
+                    .scaledToFit()
+                    .ignoresSafeArea()
+                Spacer()
+                    .frame(height:220)
+            }
+            VStack {
+                Spacer()
+                ZStack {
+                    Rectangle()
+                        .fill(
+                            LinearGradient(gradient: Gradient(stops: [
+                                Gradient.Stop(color: Color("OxfordBlue").opacity(0), location: 0),
+                                Gradient.Stop(color: Color("OxfordBlue"), location: 0.22),
+                            ]), startPoint: .top, endPoint: .bottom)
+                        )
+                        .ignoresSafeArea()
+                    HStack {
+                        Image(systemName: "chevron.compact.left")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color("LavenderBlush").opacity(0.5))
+                            .frame(width:10)
+                            .padding()
+                        Spacer()
+                        Text("Bienvenue sur Asteria !\r\rIci, le but est d’en apprendre plus sur l’Univers, ce monde **lointain et mystérieux** qui nous entoure...")
+                            .multilineTextAlignment(.center)
+                            .frame(width:180)
+                        Spacer()
+                        Image(systemName: "chevron.compact.right")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(Color("LavenderBlush").opacity(0.5))
+                            .frame(width:10)
+                            .padding()
+                    }
+                }
+                .frame(height:300)
+            }
         }
     }
 }
