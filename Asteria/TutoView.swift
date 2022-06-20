5d0db0c25f505ad8cc81eb8f1c29ddb9bf251586
Anne Ferret <anneferret94@gmail.com>
2022-06-20 11:21:42 +0200

    assets + beginning of the tuto

---
+++ Asteria/TutoView.swift
@@ -0,0 +1,25 @@
+//
+//  TutoView.swift
+//  Asteria
+//
+//  Created by apprenant70 on 20/06/2022.
+//
+
+import SwiftUI
+
+struct TutoView: View {
+    var body: some View {
+        ZStack {
+            Color.red
+                .ignoresSafeArea()
+            
+        }
+    }
+}
+
+struct TutoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TutoView()
+            .preferredColorScheme(.dark)
+    }
+}
