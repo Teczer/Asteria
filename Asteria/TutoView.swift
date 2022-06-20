b167112dbc03b409f4d44cccacd2f56ab94f4210
Anne Ferret <anneferret94@gmail.com>
2022-06-20 13:43:35 +0200

    tutorial information now are variables

--- Asteria/TutoView.swift
+++ Asteria/TutoView.swift
@@ -9,58 +9,64 @@ import SwiftUI
 import Foundation   // needed for markdown formatting in text
 
 struct TutoView: View {
+    
+    var tutoScreenNo : Int = 0
+    let tutoText : [LocalizedStringKey] = ["Bienvenue sur Asteria !\r\rIci, le but est d'en apprendre plus sur l'Univers, ce monde **lointain et mystérieux** qui nous entoure..."]
+    
     var body: some View {
-        ZStack {
-            Image("background")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-            Color("OxfordBlue").opacity(0.3)
-                .ignoresSafeArea()
-            VStack {
-                Image("tutoscreen02")
+        NavigationView {
+            ZStack {
+                Image("background")
                     .resizable()
-                    .scaledToFit()
+                    .scaledToFill()
                     .ignoresSafeArea()
-                Spacer()
-                    .frame(height:220)
-            }
-            VStack {
-                Spacer()
-                ZStack {
-                    Rectangle()
-                        .fill(
-                            LinearGradient(gradient: Gradient(stops: [
-                                Gradient.Stop(color: Color("OxfordBlue").opacity(0), location: 0),
-                                Gradient.Stop(color: Color("OxfordBlue"), location: 0.22),
-                            ]), startPoint: .top, endPoint: .bottom)
-                        )
+                Color("OxfordBlue").opacity(0.3)
+                    .ignoresSafeArea()
+                VStack {
+                    Image("tutoscreen02")
+                        .resizable()
+                        .scaledToFit()
                         .ignoresSafeArea()
-                    VStack {
-                        Spacer()
-                            .frame(height:50)
-                        HStack {
-                            Image(systemName: "chevron.compact.left")
-                                .resizable()
-                                .scaledToFit()
-                                .foregroundColor(Color("LavenderBlush").opacity(0.5))
-                                .frame(width:10)
-                                .padding()
+                    Spacer()
+                        .frame(height:240)
+                }
+                VStack {
+                    Spacer()
+                    ZStack {
+                        Rectangle()
+                            .fill(
+                                LinearGradient(gradient: Gradient(stops: [
+                                    Gradient.Stop(color: Color("OxfordBlue").opacity(0), location: 0),
+                                    Gradient.Stop(color: Color("OxfordBlue"), location: 0.25),
+                                ]), startPoint: .top, endPoint: .bottom)
+                            )
+                            .ignoresSafeArea()
+                        VStack {
                             Spacer()
-                            Text("Bienvenue sur Asteria !\r\rIci, le but est d’en apprendre plus sur l’Univers, ce monde **lointain et mystérieux** qui nous entoure...")
-                                .multilineTextAlignment(.center)
-                                .frame(width:180)
-                            Spacer()
-                            Image(systemName: "chevron.compact.right")
-                                .resizable()
-                                .scaledToFit()
-                                .foregroundColor(Color("LavenderBlush").opacity(0.5))
-                                .frame(width:10)
-                                .padding()
+                                .frame(height:40)
+                            HStack {
+                                Image(systemName: "chevron.compact.left")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(Color("LavenderBlush").opacity(0.5))
+                                    .frame(width:10)
+                                    .padding()
+                                Spacer()
+                                Text(tutoText[0])
+                                    .multilineTextAlignment(.center)
+                                    .frame(width:180)
+                                Spacer()
+                                Image(systemName: "chevron.compact.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundColor(Color("LavenderBlush").opacity(0.5))
+                                    .frame(width:10)
+                                    .padding()
+                            }
                         }
                     }
+                    .frame(height:330)
                 }
-                .frame(height:300)
             }
         }
     }
