f2a1fe24d070dda8001f4390f2eaea99cbd4806a
Anne Ferret <anneferret94@gmail.com>
2022-06-22 10:05:31 +0200

    minicard, buttons and background

--- Asteria/QuizzEndingView.swift
+++ Asteria/QuizzEndingView.swift
@@ -10,21 +10,56 @@ import SwiftUI
 struct QuizzEndingView: View {
     var body: some View {
         ZStack {
-            Color("OxfordBlue")
+            GeometryReader { geometry in
+                Image("nebuleuse4")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .clipped()
+                    .blur(radius: 5)
+            }
+            Color("SpaceCadet").opacity(0.7)
                 .ignoresSafeArea()
-            VStack {
+            VStack(spacing:0) {
                 ScoreBar()
                     .padding()
-                HStack {
+                HStack(alignment:.bottom) {
                     VStack(alignment: .leading) {
-                        Text("Bien joué")
+                        Text("Bien joué".uppercased())
+                            .font(.system(size: 22))
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color("LavenderBlush"))
                         Text("Tu remportes une carte")
+                            .font(.system(size: 16))
+                            .fontWeight(.light)
+                            .foregroundColor(Color("LavenderBlush"))
                     }
                     Spacer()
                     Text("4")
+                        .font(.system(size: 60))
+                        .fontWeight(.black)
+                        .foregroundColor(Color("LavenderBlush").opacity(0.8))
+                        .offset(y:10)
                     Text("sur \(questionNoTotal)")
+                        .font(.system(size: 20))
+                        .foregroundColor(Color("LavenderBlush").opacity(0.8))
                 }
                 .padding(.horizontal)
+                .frame(height:26)
+                Spacer()
+                Image("nebuleuse4")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width:188, height:376)
+                    .clipped()
+                    .cornerRadius(20)
+                    .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color("CarnationPink").opacity(0.7), lineWidth: 1))
+                    .shadow(color: Color("CarnationPink").opacity(0.9), radius: 32)
+                Spacer()
+                CustomButton(colorOfButton: "pink", textInButton: "Retour à l'Aventure")
+                    .padding(.bottom)
+                CustomButton(colorOfButton: "blue", textInButton: "Voir la collection")
             }
         }
     }
