1c05db5123f8dd6f76546da30591f0eb737ae735
ellanaca <ellanaca1592@gmail.com>
2022-06-23 11:07:40 +0200

    "re aventure B"

--- Asteria/struct.swift
+++ Asteria/struct.swift
@@ -27,3 +27,6 @@ let quizz: [Questions]=[
 
     Questions(questionValue: "La Lune est un satellite de la planète...", questionAnswer: ["Jupiter", "Terre", "Mars", "Ce n'est pas un satellite"], goodAnswerNo: 1, photoQuestion: "lune", photoAnswer: "terre", answerName: "Terre", answerExplanation: "La **Lune**, aussi connue sous le nom de **Terre I**, est le seul satellite de la planète Terre."),
     ]
+
+
+
