db650fbaca636633281e280c956cf8e4fe2d4434
Anne Ferret <anneferret94@gmail.com>
2022-06-23 12:35:16 +0200

    level 1 done, 4 more to go!

--- Asteria/Adventure/level1View.swift
+++ Asteria/Adventure/level1View.swift
@@ -19,38 +19,56 @@ struct level1View: View {
                 .clipped()
                 .frame(height:650)
             //
-
+            
             
             VStack {
-            
-            
-            Circle()
-                .fill(Color("OrchidCrayola").opacity(0.6))
-                .overlay(Circle().stroke(Color("OrchidCrayola"), lineWidth: 2))
-                .frame(width: 50, height: 50)
-                .shadow(color: Color("OrchidCrayola"), radius: 5)
-            
-            
-            // Level title
-            VStack(alignment: .trailing, spacing:5) {
-                Text("Niveau 1".uppercased())
-                    .font(.custom("Montserrat", size: 15))
-                    .foregroundColor(Color("LavenderBlush"))
-                    .fontWeight(.bold)
-                    .tracking(3)
-                    .offset(x:-3)
-                Text("Autour de la Terre".uppercased())
-                    .font(.custom("Montserrat", size: 22))
-                    .foregroundColor(Color("LavenderBlush"))
-                    .fontWeight(.light)
-                    .tracking(8)
-            }
-            .multilineTextAlignment(.trailing)
-            .shadow(color: Color("OxfordBlue"), radius: 128)
-            .frame(width:200)
-            .position(x: 240, y: 520)
-            //
-            
+                
+                ZStack {
+                    
+                    /// **DOCUMENTATION pour LevelCircle**
+                    ///  **levelStatus** peut avoir 3 valeurs :
+                    ///      *"completed"*      niveaux déjà terminés
+                    ///      *"unavailable"*     niveaux à venir (pas encore débloqués)
+                    ///      *"new"*                prochain niveau à jouer (il ne doit y en avoir qu'un seul à tout moment)
+                    ///  **circleSize** est une valeur Int entre 30 et 50 qui change la taille du point
+                    
+                    LevelCircle(levelStatus: "completed", circleSize: 40)
+                        .position(x: 75, y: 400)
+                    
+                    LevelCircle(levelStatus: "completed", circleSize: 30)
+                        .position(x: 150, y: 340)
+
+                    LevelCircle(levelStatus: "new", circleSize: 30)
+                        .position(x: 225, y: 300)
+                    
+                    LevelCircle(levelStatus: "unavailable", circleSize: 30)
+                        .position(x: 290, y: 250)
+                    
+                    LevelCircle(levelStatus: "unavailable", circleSize: 50)
+                        .position(x: 270, y: 185)
+                    
+                    // Level title
+                    VStack(alignment: .trailing, spacing:5) {
+                        Text("Niveau 1".uppercased())
+                            .font(.custom("Montserrat", size: 15))
+                            .foregroundColor(Color("LavenderBlush"))
+                            .fontWeight(.bold)
+                            .tracking(3)
+                            .offset(x:-3)
+                        Text("Autour de la Terre".uppercased())
+                            .font(.custom("Montserrat", size: 22))
+                            .foregroundColor(Color("LavenderBlush"))
+                            .fontWeight(.light)
+                            .tracking(8)
+                    }
+                    .multilineTextAlignment(.trailing)
+                    .shadow(color: Color("OxfordBlue"), radius: 128)
+                    .frame(width:200)
+                    .position(x: 240, y: 520)
+                    //
+                    
+                }
+                
             }
             .frame(height:650)
             .clipped()
---
+++ Asteria/DevFeatures/LevelCircle.swift
@@ -0,0 +1,63 @@
+//
+//  LevelCircle.swift
+//  Asteria
+//
+//  Created by apprenant70 on 23/06/2022.
+//
+
+import SwiftUI
+
+struct LevelCircle: View {
+    
+    /// **DOCUMENTATION pour LevelCircle**
+    ///  **levelStatus** peut avoir 3 valeurs :
+    ///      *"completed"*      niveaux déjà terminés
+    ///      *"unavailable"*     niveaux à venir (pas encore débloqués)
+    ///      *"new"*                prochain niveau à jouer (il ne doit y en avoir qu'un seul à tout moment)
+    ///  **circleSize** est une valeur Int entre 30 et 50 qui change la taille du point
+
+    let levelStatus : String
+    let circleSize : CGFloat
+    
+    var body: some View {
+        
+        if levelStatus == "completed" {
+            ZStack {
+                Circle()
+                    .fill(Color("CarnationPink").opacity(0.9))
+                    .overlay(Circle().stroke(Color("CarnationPink"), lineWidth: 2))
+                    .frame(width: circleSize, height: circleSize)
+                    .shadow(color: Color("CarnationPink"), radius: 5)
+                Image(systemName: "checkmark")
+                    .font(.system(size: 18))
+            }
+        }
+        
+        else if levelStatus == "unavailable" {
+            Circle()
+                .fill(Color("OrchidCrayola").opacity(0.5))
+                .overlay(Circle().stroke(Color("CarnationPink").opacity(0.8), lineWidth: 2))
+                .frame(width: circleSize, height: circleSize)
+                .shadow(color: Color("OrchidCrayola"), radius: 5)
+        }
+        
+        else if levelStatus == "new" {
+            Circle()
+                .fill(Color("LavenderBlush").opacity(0.9))
+                .overlay(Circle().stroke(Color("LavenderBlush"), lineWidth: 2))
+                .frame(width: circleSize, height: circleSize)
+                .shadow(color: Color("LavenderBlush"), radius: 10)
+        }
+        
+        else {
+            Text("Erreur : LevelCircle a reçu une valeur inattendue")
+        }
+                
+    }
+}
+
+struct LevelCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        LevelCircle(levelStatus: "unavailable", circleSize: 30)
+    }
+}
