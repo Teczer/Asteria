e9b51cc01d3a070715f743d3892892ffbb3e3762
Anne Ferret <anneferret94@gmail.com>
2022-06-23 10:48:51 +0200

    (inactive) back button on SingleCardView

--- Asteria/SingleCardView.swift
+++ Asteria/SingleCardView.swift
@@ -28,63 +28,80 @@ struct SingleCardView: View {
         ZStack {
             BlurredBackground(name: image)
             
-            
-            if displayBack {
-                CollectionCardBack(collectionCardBack: CardBack(
-                    cardBackImage: image,
-                    cardTitle: cardTitle,
-                    cardFunFactIcon1: "bolt.circle.fill",
-                    cardFunFactIcon2: "bolt.circle.fill",
-                    cardFunFactIcon3: "bolt.circle.fill",
-                    cardFunFactName1: "Spec 1 :",
-                    cardFunFactName2: "Spec 2 :",
-                    cardFunFactName3: "Spec 3 :",
-                    cardFunFact1: """
-                                        Chiffre
-                                        ou fun fact
-                                        """,
-                    cardFunFact2: """
-                                        Chiffre
-                                        ou fun fact
-                                        """,
-                    cardFunFact3: """
-                                        Chiffre
-                                        ou fun fact
-                                        """,
-                    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue placerat quam id blandit. Cras maximus tempor efficitur. Curabitur leo metus, porta vel blandit vitae, suscipit et ex. Etiam ac rutrum mauris, sed euismod magna. Sed a porta urna. Nullam vehicula fermentum facilisis. Pellentesque gravida, nisl a consectetur mattis, magna urna pretium elit, eget euismod enim sapien quis orci. Proin quis tellus eros."))
-                .onTapGesture {
-                        displayFront = true
-                        withAnimation(.linear(duration: 0.2)) {
-                            rotationBackValue = 90
-                        }
-                        withAnimation(.linear(duration: 0.2).delay(0.2)) {
-                            rotationFrontValue = 0
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                            displayBack = false
-                        }
+            VStack {
+                HStack(alignment:.top) {
+                    Image(systemName: "chevron.backward.square.fill")
+                        .foregroundColor(Color("LavenderBlush").opacity(0.8))
+                        .font(.system(size: 32))
+                        .padding()
+                    Spacer()
                 }
-                .rotation3DEffect(.degrees(rotationBackValue), axis: (x: 0, y: 1, z: 0))
-            }
-            
-            
-            if displayFront {
-                CollectionCardFront(collectionCardFront: CardFront(cardFrontImage: cardImage, cardTitle: cardTitle, cardNumber: cardNumber, collectionName: collectionName, miniCard: miniCard))
-                    .onTapGesture {
-                            displayBack = true
+                
+                Spacer()
+                    .frame(height:30)
+                
+                ZStack {
+                    
+                    if displayBack {
+                        
+                        CollectionCardBack(collectionCardBack: CardBack(
+                            cardBackImage: image,
+                            cardTitle: cardTitle,
+                            cardFunFactIcon1: "bolt.circle.fill",
+                            cardFunFactIcon2: "bolt.circle.fill",
+                            cardFunFactIcon3: "bolt.circle.fill",
+                            cardFunFactName1: "Spec 1 :",
+                            cardFunFactName2: "Spec 2 :",
+                            cardFunFactName3: "Spec 3 :",
+                            cardFunFact1: """
+                                        Chiffre
+                                        ou fun fact
+                                        """,
+                            cardFunFact2: """
+                                        Chiffre
+                                        ou fun fact
+                                        """,
+                            cardFunFact3: """
+                                        Chiffre
+                                        ou fun fact
+                                        """,
+                            cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue placerat quam id blandit. Cras maximus tempor efficitur. Curabitur leo metus, porta vel blandit vitae, suscipit et ex. Etiam ac rutrum mauris, sed euismod magna. Sed a porta urna. Nullam vehicula fermentum facilisis. Pellentesque gravida, nisl a consectetur mattis, magna urna pretium elit, eget euismod enim sapien quis orci. Proin quis tellus eros."))
+                        .onTapGesture {
+                            displayFront = true
                             withAnimation(.linear(duration: 0.2)) {
-                                rotationFrontValue = -90
+                                rotationBackValue = 90
                             }
                             withAnimation(.linear(duration: 0.2).delay(0.2)) {
-                                rotationBackValue = 0
+                                rotationFrontValue = 0
                             }
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                displayFront = false
+                                displayBack = false
                             }
                         }
-                    .rotation3DEffect(.degrees(rotationFrontValue), axis: (x: 0, y: 1, z: 0))
+                        .rotation3DEffect(.degrees(rotationBackValue), axis: (x: 0, y: 1, z: 0))
+                    }
+                    
+                    
+                    if displayFront {
+                        CollectionCardFront(collectionCardFront: CardFront(cardFrontImage: cardImage, cardTitle: cardTitle, cardNumber: cardNumber, collectionName: collectionName, miniCard: miniCard))
+                            .onTapGesture {
+                                displayBack = true
+                                withAnimation(.linear(duration: 0.2)) {
+                                    rotationFrontValue = -90
+                                }
+                                withAnimation(.linear(duration: 0.2).delay(0.2)) {
+                                    rotationBackValue = 0
+                                }
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                    displayFront = false
+                                }
+                            }
+                            .rotation3DEffect(.degrees(rotationFrontValue), axis: (x: 0, y: 1, z: 0))
+                    }
+                }
+                
+                Spacer()
             }
-            
         }
         
     }
