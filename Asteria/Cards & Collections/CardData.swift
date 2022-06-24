c16ae1380d5c7409c18b8526822db595691ff31f
LeiaStarkiller <margaux.hess@gmail.com>
2022-06-24 16:57:02 +0200

    collection card data

--- Asteria/Cards & Collections/CardData.swift
+++ Asteria/Cards & Collections/CardData.swift
@@ -16,7 +16,7 @@ let cardBacks = [cardBack1, cardBack2]
 // COLLECTIONS
 let collection1 = CollectionType(
     collectionName: "S Y S T È M E  S O L A I R E",
-    collectionImages: [SingleCardType(cardFront: cardFront1, cardBack: cardBack1)])
+    collectionImages: [SingleCardType(cardFront: cardFront1, cardBack: cardBack1), SingleCardType(cardFront: cardFront2, cardBack: cardBack2), SingleCardType(cardFront: cardFront3, cardBack: cardBack3), SingleCardType(cardFront: cardFront4, cardBack: cardBack4)])
 
 let collection2 = CollectionType(
     collectionName: "G A L A X I E S",
@@ -61,61 +61,148 @@ let cardBack1 = CardBackType(
     cardDescription: "Le Soleil est une étoile de type naine jaune, principalement composée d’hydrogène (74%) et d’hélium (25%), et âgée de 4,5 millards d’années. Le nom Soleil vient du latin classique « sol », qui désigne l’astre, et « solis » renvoyant au Soleil en tant que divinité. Sa surface n’est pas solide mais constituée de gaz, qu’on appelle photosphère et qui émet sa lumière la plus visible. Son noyau peut atteindre 15 millions de degrés Celsius.")
 
 let cardFront2 = CardFrontType(
-    cardTitle: "L U N E",
-    cardFrontImage: "collection1-image-7",
-    collectionName: "G A L A X I E S",
+    cardTitle: "C O M È T E",
+    cardFrontImage: "collec1-cardFrontImage-2-comet",
+    collectionName: "S Y S T È M E  S O L A I R E",
     cardNumber: "2",
     miniCard: false)
 
 let cardBack2 = CardBackType(
-    cardTitle: "L U N E",
-    cardBackImage: "collection1-image-7",
-    cardFunFactIcon1: "bolt.circle.fill",
-    cardFunFactIcon2: "bolt.circle.fill",
-    cardFunFactIcon3: "bolt.circle.fill",
-    cardFunFactName1: "Spec 1 :",
-    cardFunFactName2: "Spec 2 :",
-    cardFunFactName3: "Spec 3 :",
+    cardTitle: "C O M È T E",
+    cardBackImage: "collec1-cardBackImage-2-comet",
+    cardFunFactIcon1: "binoculars.fill",
+    cardFunFactIcon2: "bonjour",
+    cardFunFactIcon3: "ruler.fill",
+    cardFunFactName1: """
+                        Premières
+                        observations :
+                        """,
+    cardFunFactName2: """
+                        Comètes
+                        répertoriées :
+                        """,
+    cardFunFactName3: """
+                        Plus longue
+                        traînée :
+                        """,
     cardFunFact1: """
-                    Chiffre
-                    ou fun fact
+                    1059
+                    av. J.C.
                     """,
     cardFunFact2: """
-                    Chiffre
-                    ou fun fact
+                    4 352
+                    au 14 janvier 2020
                     """,
     cardFunFact3: """
-                    Chiffre
-                    ou fun fact
+                    580 millions
+                    de kilomètres
                     """,
-    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue placerat quam id blandit. Cras maximus tempor efficitur. Curabitur leo metus, porta vel blandit vitae, suscipit et ex. Etiam ac rutrum mauris, sed euismod magna. Sed a porta urna. Nullam vehicula fermentum facilisis. Pellentesque gravida, nisl a consectetur mattis, magna urna pretium elit, eget euismod enim sapien quis orci. Proin quis tellus eros.")
+    cardDescription: "Une comète est un petit corps céleste constitué d’un noyau de glace et de poussière en orbite autour d’une étoile. Lorsqu’elle s’approche de cette étoile, le noyau s’entoure d’une sorte de fine atmosphère brillante constituée de gaz et de poussières, appelée chevelure, souvent prolongée de deux traînées lumineuses, qu’on appelle les queues, et qui peuvent s’étendre sur plusieurs centaines de millions de km. Le mot « comète » vient du grec ancien et signifie « astre chevelu ». ")
 
 let cardFront3 = CardFrontType(
-    cardTitle: "A R M S T R O N G",
-    cardFrontImage: "level4",
-    collectionName: "A S T R O N A U T E S  C É L È B R E S",
+    cardTitle: "N E P T U N E",
+    cardFrontImage: "collec1-cardFrontImage-3-neptune",
+    collectionName: "S Y S T È M E  S O L A I R E",
     cardNumber: "3",
     miniCard: false)
 
 let cardBack3 = CardBackType(
-    cardTitle: "A R M S T R O N G",
-    cardBackImage: "level4",
-    cardFunFactIcon1: "bolt.circle.fill",
-    cardFunFactIcon2: "bolt.circle.fill",
-    cardFunFactIcon3: "bolt.circle.fill",
-    cardFunFactName1: "Spec 1 :",
-    cardFunFactName2: "Spec 2 :",
-    cardFunFactName3: "Spec 3 :",
+    cardTitle: "N E P T U N E",
+    cardBackImage: "collec1-cardBackImage-3-neptune",
+    cardFunFactIcon1: "circle.slash.fill",
+    cardFunFactIcon2: "arrow.left.and.right.circle.fill",
+    cardFunFactIcon3: "thermometer",
+    cardFunFactName1: "Diamètre :",
+    cardFunFactName2: "Distance de la terre :",
+    cardFunFactName3: "Température :",
     cardFunFact1: """
-                    Chiffre
-                    ou fun fact
+                    49 244 km
+                    soit 3,9 fois la Terre
                     """,
     cardFunFact2: """
-                    Chiffre
-                    ou fun fact
+                    4,3 milliards
+                    de kilomètres
                     """,
     cardFunFact3: """
-                    Chiffre
-                    ou fun fact
+                    −218,15 °C
+                    en surface
                     """,
-    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue placerat quam id blandit. Cras maximus tempor efficitur. Curabitur leo metus, porta vel blandit vitae, suscipit et ex. Etiam ac rutrum mauris, sed euismod magna. Sed a porta urna. Nullam vehicula fermentum facilisis. Pellentesque gravida, nisl a consectetur mattis, magna urna pretium elit, eget euismod enim sapien quis orci. Proin quis tellus eros.")
+    cardDescription: "Neptune est la planète la plus éloignée du système solaire. Les conditions particulières de Neptune transformeraient le carbone présent en diamant. Ces diamants tomberaient ensuite vers le noyau, donnant naissance à une pluie de nanodiamants. Invisible à l'œil nu, Neptune est la première planète du système solaire à avoir été découverte par déduction plutôt que par observation.")
+
+let cardFront4 = CardFrontType(
+    cardTitle: "A S T É R O Ï D E",
+    cardFrontImage: "collec1-cardFrontImage-4-asteroid",
+    collectionName: "S Y S T È M E  S O L A I R E",
+    cardNumber: "4",
+    miniCard: false)
+
+let cardBack4 = CardBackType(
+    cardTitle: "A S T É R O Ï D E",
+    cardBackImage: "collec1-cardBackImage-4-asteroid",
+    cardFunFactIcon1: "binoculars.fill",
+    cardFunFactIcon2: "bonjour",
+    cardFunFactIcon3: "gyroscope",
+    cardFunFactName1: """
+                        Première
+                        découverte :
+                        """,
+    cardFunFactName2: """
+                        Astéroïdes
+                        répertoriés :
+                        """,
+    cardFunFactName3: """
+                        Première
+                        exploration :
+                        """,
+    cardFunFact1: """
+                    1801
+                    Astéroïde Cérès
+                    """,
+    cardFunFact2: """
+                    plus de
+                    720 000
+                    """,
+    cardFunFact3: """
+                    Sonde Galileo
+                    en 1991
+                    """,
+    cardDescription: "Un astéroïde est un corps céleste composé de roches et de métaux, dont la taille peut aller de quelques centimètres à plusieurs kilomètres. La majorité se situe dans la zone comprise entre Mars et Jupiter, appelée la ceinture d’astéroïdes. Les astéroïdes dits géocroiseurs évoluent proches de la Terre, et sont étroitement surveillés afin de prévenir toute collision avec la Terre. On en dénombre plus de 20 000.")
+
+let cardFront5 = CardFrontType(
+    cardTitle: "S A T U R N E",
+    cardFrontImage: "collec1-cardFrontImage-5-saturn",
+    collectionName: "S Y S T È M E  S O L A I R E",
+    cardNumber: "4",
+    miniCard: false)
+
+let cardBack5 = CardBackType(
+    cardTitle: "S A T U R N E",
+    cardBackImage: "collec1-cardBackImage-5-saturn",
+    cardFunFactIcon1: "binoculars.fill",
+    cardFunFactIcon2: "bonjour",
+    cardFunFactIcon3: "gyroscope",
+    cardFunFactName1: """
+                        Première
+                        découverte :
+                        """,
+    cardFunFactName2: """
+                        Astéroïdes
+                        répertoriés :
+                        """,
+    cardFunFactName3: """
+                        Première
+                        exploration :
+                        """,
+    cardFunFact1: """
+                    1801
+                    Astéroïde Cérès
+                    """,
+    cardFunFact2: """
+                    plus de
+                    720 000
+                    """,
+    cardFunFact3: """
+                    Sonde Galileo
+                    en 1991
+                    """,
+    cardDescription: "Un astéroïde est un corps céleste composé de roches et de métaux, dont la taille peut aller de quelques centimètres à plusieurs kilomètres. La majorité se situe dans la zone comprise entre Mars et Jupiter, appelée la ceinture d’astéroïdes. Les astéroïdes dits géocroiseurs évoluent proches de la Terre, et sont étroitement surveillés afin de prévenir toute collision avec la Terre. On en dénombre plus de 20 000.")
